bab52993907adcf60fa78ff6c05ed12d10685d2f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-31 12:08:04 +0200

    Wait for `launch` before creating widgets

--- example/main.js
+++ example/main.js
@@ -4,45 +4,47 @@ const { App, Window, WebView } = require('..')
 
 const app = App.shared()
 
-const window = new Window(0, 0, 500, 500)
-
-window
-  .on('move', (x, y) => {
-    console.log('move', x, y)
-  })
-  .on('resize', (width, height) => {
-    console.log('resize', width, height)
-
-    webview.setBounds(0, 0, width, height)
-  })
-  .on('close', () => {
-    console.log('close')
-
-    app.terminate()
-  })
-
-const webview = new WebView(path.join(__dirname, 'webview'), 0, 0, 500, 500)
-
-webview
-  .on('ready', () => {
-    webview.loadHTML(`
-      <style>body { font-family: system-ui }</style>
-      <p>Hello world!<p>
-      <script>
-      bridge.postMessage({ from: 'WebView' })
-      </script>
-    `)
-  })
-  .on('message', (message) => {
-    console.log(message)
-  })
-
-window
-  .appendChild(webview)
-  .show()
-
 app
-  .on('launch', () => new Worker(require.resolve('./background')))
+  .on('launch', () => {
+    const window = new Window(0, 0, 500, 500)
+
+    window
+      .on('move', (x, y) => {
+        console.log('move', x, y)
+      })
+      .on('resize', (width, height) => {
+        console.log('resize', width, height)
+
+        webview.setBounds(0, 0, width, height)
+      })
+      .on('close', () => {
+        console.log('close')
+
+        app.terminate()
+      })
+
+    const webview = new WebView(path.join(__dirname, 'webview'), 0, 0, 500, 500)
+
+    webview
+      .on('ready', () => {
+        webview.loadHTML(`
+          <style>body { font-family: system-ui }</style>
+          <p>Hello world!<p>
+          <script>
+          bridge.postMessage({ from: 'WebView' })
+          </script>
+        `)
+      })
+      .on('message', (message) => {
+        console.log(message)
+      })
+
+    window
+      .appendChild(webview)
+      .show()
+
+    return new Worker(require.resolve('./background'))
+  })
   .on('terminate', () => {
     console.log('terminate')
 
