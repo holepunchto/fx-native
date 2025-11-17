94b4ecb601d4ce893d733b1edbde908eea116fa2
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-17 20:40:32 +0100

    Update `libfx`

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 exports.App = require('./lib/app')
-
-const app = exports.App.shared()
-
-if (app.isMain) {
-  exports.Image = require('./lib/image')
-  exports.Text = require('./lib/text')
-  exports.View = require('./lib/view')
-  exports.Screen = require('./lib/screen')
-  exports.WebView = require('./lib/web-view')
-  exports.Window = require('./lib/window')
-}
+exports.Image = require('./lib/image')
+exports.Text = require('./lib/text')
+exports.View = require('./lib/view')
+exports.Screen = require('./lib/screen')
+exports.WebView = require('./lib/web-view')
+exports.Window = require('./lib/window')
